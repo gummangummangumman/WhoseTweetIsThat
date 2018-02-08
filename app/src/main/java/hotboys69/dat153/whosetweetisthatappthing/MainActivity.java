74b6fd42afe4ae2c369eed852b94b87945403b9e
Emilracerbil <emilracerbil@gmail.com>
2018-02-08 16:11:11 +0100

    Twitter Auth

--- app/src/main/java/hotboys69/dat153/whosetweetisthatappthing/MainActivity.java
+++ app/src/main/java/hotboys69/dat153/whosetweetisthatappthing/MainActivity.java
@@ -2,6 +2,12 @@ package hotboys69.dat153.whosetweetisthatappthing;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.twitter.sdk.android.core.DefaultLogger;
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterConfig;
 
 import java.util.ArrayList;
 
@@ -11,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Twitter.initialize(this);
+
 
         ArrayList<String> tweets = JsonConnect.getAllTweets(Data.users.get(0));
     }
