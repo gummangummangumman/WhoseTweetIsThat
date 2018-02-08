48fc4c564d6dbe807a8d36b2512a0e20a77e839f
GuMMaN <trygve@igesund.com>
2018-02-08 13:19:19 +0100

    Initial commit

---
+++ app/src/main/java/hotboys69/dat153/whosetweetisthatappthing/MainActivity.java
@@ -0,0 +1,13 @@
+package hotboys69.dat153.whosetweetisthatappthing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
