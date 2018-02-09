374c723394509058360dc6ef9b989d63ec798360
Emilracerbil <emilracerbil@gmail.com>
2018-02-09 14:56:41 +0100

    activities

---
+++ app/src/main/java/hotboys69/dat153/whosetweetisthatappthing/LoseActivity.java
@@ -0,0 +1,13 @@
+package hotboys69.dat153.whosetweetisthatappthing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lose);
+    }
+}
