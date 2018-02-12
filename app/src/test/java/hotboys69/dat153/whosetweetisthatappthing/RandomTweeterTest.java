46f4d1a8a5f3e36dcb41e1015136d5eabd23ee49
GuMMaN <trygve@igesund.com>
2018-02-12 16:53:05 +0100

    testin some of that logic

---
+++ app/src/test/java/hotboys69/dat153/whosetweetisthatappthing/RandomTweeterTest.java
@@ -0,0 +1,42 @@
+package hotboys69.dat153.whosetweetisthatappthing;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by GuMMaN on 12.02.2018.
+ */
+
+public class RandomTweeterTest {
+
+    public ArrayList<String> tweeters;
+
+    @Before
+    public void setup(){
+        tweeters = RandomTweeters.getRandomTweeters();
+    }
+
+    @Test
+    public void theSizeIsFour() {
+        tweeters = RandomTweeters.getRandomTweeters();
+        assertEquals(tweeters.size(), 4);
+    }
+
+    @Test
+    public void usernameExists(){
+        String username = RandomTweeters.getRandomTweeter(tweeters);
+        assertTrue(Data.musicians.contains(username) || Data.users.contains(username));
+    }
+
+    @After
+    public void cleanup(){
+        tweeters = null;
+    }
+
+}
