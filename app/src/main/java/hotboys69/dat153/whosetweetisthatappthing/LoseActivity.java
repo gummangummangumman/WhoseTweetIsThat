2f102bc5bcaa09e3de7fb3d0038f35113417f690
Emilracerbil <emilracerbil@gmail.com>
2018-02-14 16:09:23 +0100

    LoseActivity

--- app/src/main/java/hotboys69/dat153/whosetweetisthatappthing/LoseActivity.java
+++ app/src/main/java/hotboys69/dat153/whosetweetisthatappthing/LoseActivity.java
@@ -1,13 +1,59 @@
 package hotboys69.dat153.whosetweetisthatappthing;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoseActivity extends AppCompatActivity {
+
+    int score;
+
+    TextView scoreLoseText, gameOverText;
+
+    Button playAgainButton, mainMenuButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lose);
+
+        scoreLoseText = findViewById(R.id.scoreLoseText);
+        gameOverText = findViewById(R.id.gameOverText);
+
+        score = 0;
+
+        gameOverText.setText("Game Over");
+        scoreLoseText.setText("You got a score of: " + score);
+
+
+        playAgainButton = findViewById(R.id.playAgainButton);
+        mainMenuButton = findViewById(R.id.mainMenuButton);
+
+        playAgainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startGame();
+            }
+        });
+
+        mainMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mainMenu();
+            }
+        });
+    }
+
+    private void mainMenu() {
+        Intent menuIntent = new Intent(this, MainActivity.class);
+        startActivity(menuIntent);
+    }
+
+    public void startGame() {
+        Intent gameIntent = new Intent(this, GameActivity.class);
+        startActivity(gameIntent);
     }
 }
