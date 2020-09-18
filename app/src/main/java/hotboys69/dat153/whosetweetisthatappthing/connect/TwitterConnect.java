2ea8d613094c70ba683934a51d4bfb8133eda5f7
gummangummangumman <trygve@igesund.com>
2020-09-18 22:50:20 +0200

    Use better error handling to prevent app crashes

--- app/src/main/java/hotboys69/dat153/whosetweetisthatappthing/connect/TwitterConnect.java
+++ app/src/main/java/hotboys69/dat153/whosetweetisthatappthing/connect/TwitterConnect.java
@@ -26,7 +26,7 @@ public class TwitterConnect {
      * @param username the @ of the user
      * @param view GameActivity
      */
-    public static void setUserInformation(String username, GameActivity view)
+    public static void setUserInformation(final String username, GameActivity view)
     {
         final WeakReference<GameActivity> callback = new WeakReference<GameActivity>(view);
         TwitterApiClient twitterApiClient = TwitterCore.getInstance().getApiClient();
@@ -36,7 +36,13 @@ public class TwitterConnect {
             @Override
             public void success(Result<List<Tweet>> result) {
 
-                if(result.data.get(0).user.screenName.equals(callback.get().correctUserName))
+                if(result.data.size() == 0)
+                {
+                    Log.e("NO_TWEETS", "No tweets from user @" + username);
+                    callback.get().setFailed();
+                    return;
+                }
+                else if(result.data.get(0).user.screenName.equals(callback.get().correctUserName))
                 {
                     try{
                         String randomTweet = RandomTweetPicker.getRandomTweet(result.data);
@@ -44,14 +50,16 @@ public class TwitterConnect {
                     } catch (Resources.NotFoundException e){
                         e.printStackTrace();
                         callback.get().setFailed();
+                        return;
                     }
                 }
-
                 callback.get().setUserInformation(result.data.get(0).user.name, result.data.get(0).user.profileImageUrl, result.data.get(0).user.screenName);
             }
 
             @Override
             public void failure(TwitterException exception) {
+                Log.e("TWITTER_CONNECT_ERROR", exception.getMessage());
+                exception.printStackTrace();
                 callback.get().setFailed();
             }
         });
